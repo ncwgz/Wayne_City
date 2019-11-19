24bb27d4445236c3b1fc47a6fbd2f2b5ad58a22c
guozhi <wgz615402729@live.com>
2019-11-18 20:55:06 -0500

    update

--- redblacktree.h
+++ redblacktree.h
@@ -54,28 +54,63 @@ private:
         if (parent->data > to_append->data) {
             if (parent->left == nullptr) {
                 parent->left = to_append;
+                insertFix(to_append);
             } else {
                 append(to_append, parent->left);
             }
         } else {
             if (parent->right == nullptr) {
                 parent->right = to_append;
+                insertFix(to_append);
             } else {
                 append(to_append, parent->right);
             }
         }
     }
 
+    void rootRedCheck() {
+        if (this->root->isRed) {
+            this->root->isRed = false;
+        }
+    }
+
     void insertFix(Node<T> *node) {
+        // Two continuous red nodes
         while (node->parent->isRed) {
-            if (node->parent = node->parent->parent->left) {
+            if (node->parent == node->parent->parent->left) {
+                // The color-change case
                 if (node->parent->parent->right->isRed) {
                     node->parent->isRed = false;
                     node->parent->parent->right->isRed = false;
                     node->parent->parent->isRed = true;
                     node = node->parent->parent;
                 } else {
-                    
+                    // The LL-rotate case
+                    if (node->parent->left == node) {
+                        rightRotate(node->parent->parent);
+                        rootRedCheck();
+                    }
+                    // The LR-rotate case
+                    else {
+
+                    }
+                }
+            } else {
+                // The color-change case
+                if (node->parent->parent->left->isRed) {
+                    node->parent->isRed = false;
+                    node->parent->parent->left->isRed = false;
+                    node->parent->parent->isRed = true;
+                } else {
+                    // The RR-rotate case
+                    if (node->parent->right == node) {
+                        leftRotate(node->parent->parent);
+                        rootRedCheck();
+                    }
+                    // The RL-rotate case
+                    else {
+
+                    }
                 }
             }
         }
